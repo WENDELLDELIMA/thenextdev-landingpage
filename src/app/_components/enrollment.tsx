171ced46c2119db30586daebf2fb5013718191a1
Wendell lima <wendell.lima22@hotmail.com>
2024-02-06 17:54:19 -0300

    Alteracao da pagina about

--- src/app/_components/enrollment.tsx
+++ src/app/_components/enrollment.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 function Enrollment() {
   return (
     <Link
-      href={"https://google.com.br"}
+      href={"https://www.linkedin.com/in/wendell-de-lima/"}
       target="_blank"
       className="bg-primary hover:bg-primary text-white font-bold py-2 px-4 rounded shadow-lg hover:shadow-xl transition-all duration-200 transform hover:scale-105"
     >
